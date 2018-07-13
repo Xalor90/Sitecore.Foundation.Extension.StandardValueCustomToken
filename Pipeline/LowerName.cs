3d496a59168a4976545a8e0ccd7cf94a67ed5cc9
Derek Temple <xalor90@outlook.com>
2018-07-13 13:17:48 -0700

    Set up project and introduced custom token to pipeline

---
+++ Pipeline/LowerName.cs
@@ -0,0 +1,16 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Pipelines.ExpandInitialFieldValue;
+
+namespace Sitecore.Foundation.Extension.StandardValueCustomToken.Pipeline
+{
+    public class LowerName : ExpandInitialFieldValueProcessor
+    {
+        private const string Token = "$lowerName";
+
+        public override void Process(ExpandInitialFieldValueArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            args.Result = args.Result.Replace(Token, args.TargetItem.Name.ToLower().Replace(" ", "-"));
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sitecore.Foundation.Extension.StandardValueCustomToken")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("XalorTech")]
+[assembly: AssemblyProduct("Sitecore.Foundation.Extension.StandardValueCustomToken")]
+[assembly: AssemblyCopyright("Copyright © XalorTech 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b8938df0-a45d-40e5-8df8-51001a649cba")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
